53a24e52ac778ba8884e71490a4464148a29fa83
Javier (Home) <terkitos@gmail.com>
2016-12-20 00:26:57 +0100

    Added comments to the sample

--- sample/src/main/kotlin/com/jdiazcano/sample/Sample.kt
+++ sample/src/main/kotlin/com/jdiazcano/sample/Sample.kt
@@ -4,17 +4,29 @@ import com.jdiazcano.konfig.loaders.PropertyConfigLoader
 import com.jdiazcano.konfig.providers.DefaultConfigProvider
 
 fun main(args: Array<String>) {
-    val loader = PropertyConfigLoader(DatabaseConfig::class.java.getResource("/global.properties"))
-    val provider = DefaultConfigProvider(loader)
-    val databaseConfig = provider.bind("database", DatabaseConfig::class.java)
+    val loader = PropertyConfigLoader(DatabaseConfig::class.java.getResource("/global.properties")) // Create loader
+    val provider = DefaultConfigProvider(loader)                                                    // Create provider
+    val databaseConfig = provider.bind("database", DatabaseConfig::class.java)                      // bind and use
 
     println("Name: ${databaseConfig.name()}")
     println("Url: ${databaseConfig.url()}")
     println("Port: ${databaseConfig.port()}")
 }
 
+/**
+ * This interface defines a database configuration
+ */
 interface DatabaseConfig {
+    /**
+     * You can have javadocs inside your properties and this is really cool
+     */
     fun url(): String
     fun port(): Int
     fun name(): String
+
+    // if you have an unused property you know it and you can delete it
+    fun unused(): String
+
+    @Deprecated("You can even deprecate properties!")
+    fun deprecated(): Boolean
 }
