58f2a3768c067e0ae9d69ce220364d9c33d45cd0
Javier Diaz-Cano <terkitos@gmail.com>
2016-12-24 00:30:07 +0100

    OverrideConfigProvider seems to be working! Now you can pass a list of Loaders into a Provider and it will choose (in normal order of precedence) the first suitable property for you!

---
+++ src/test/kotlin/com/jdiazcano/konfig/OverrideConfigProviderTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015-2016 Javier Díaz-Cano Martín-Albo (javierdiazcanom@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jdiazcano.konfig
+
+import com.jdiazcano.konfig.loaders.PropertyConfigLoader
+import com.jdiazcano.konfig.providers.OverrideConfigProvider
+import com.winterbe.expekt.should
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class OverrideConfigProviderTest : Spek({
+    val provider = OverrideConfigProvider(
+            PropertyConfigLoader(javaClass.classLoader.getResource("overridetest.properties")),
+            PropertyConfigLoader(javaClass.classLoader.getResource("test.properties"))
+    )
+    describe("An overriding provider") {
+        it("if the property exist in the first, should not go to the second loader") {
+            provider.getProperty("a", String::class.java).should.be.equal("overrideb")
+            provider.getProperty("c", String::class.java).should.be.equal("overrided")
+        }
+
+        it("if the property does not exist, then the second one should be tested") {
+            provider.getProperty("integerProperty", Int::class.java).should.be.equal(1)
+        }
+    }
+})
