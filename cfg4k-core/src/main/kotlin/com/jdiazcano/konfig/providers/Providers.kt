d6f05bd7d9362578af63307fa47b55027da0ce6e
jdiazcano <terkitos@gmail.com>
2017-01-07 21:48:24 +0100

    Added HOCON ConfigLoader with tests

---
+++ cfg4k-core/src/main/kotlin/com/jdiazcano/konfig/providers/Providers.kt
@@ -0,0 +1,15 @@
+package com.jdiazcano.konfig.providers
+
+import com.jdiazcano.konfig.ConfigLoader
+import com.jdiazcano.konfig.ConfigProvider
+import com.jdiazcano.konfig.loaders.ReloadStrategy
+
+class Providers {
+    companion object {
+        fun cached(provider: ConfigProvider) = CachedConfigProvider(provider)
+
+        fun overriden(loaders: Array<ConfigLoader>, reloadStrategy: ReloadStrategy? = null) = OverrideConfigProvider(loaders, reloadStrategy)
+
+        fun proxy(configLoader: ConfigLoader, reloadStrategy: ReloadStrategy? = null) = ProxyConfigProvider(configLoader, reloadStrategy)
+    }
+}
