c4534ff574cb9179eba4f3bc17f15a335671f6d2
jdiazcano <terkitos@gmail.com>
2017-01-18 00:00:18 +0100

    Much better system property loader, let the System do our work isntead!

--- cfg4k-core/src/main/kotlin/com/jdiazcano/konfig/loaders/SystemPropertyConfigLoader.kt
+++ cfg4k-core/src/main/kotlin/com/jdiazcano/konfig/loaders/SystemPropertyConfigLoader.kt
@@ -3,25 +3,12 @@ package com.jdiazcano.konfig.loaders
 import com.jdiazcano.konfig.ConfigLoader
 
 open class SystemPropertyConfigLoader : ConfigLoader {
-    protected val properties: MutableMap<String, String> = mutableMapOf()
-
-    init {
-        loadProperties()
-    }
-
-    private fun loadProperties() {
-        properties.clear()
-        System.getProperties().forEach { key, value ->
-            properties[key.toString()] = value.toString()
-        }
+    override fun reload() {
+        // Nothing to do, the System.properties do the reload for us!
     }
 
     override fun get(key: String): String {
-        return properties[key]?:""
-    }
-
-    override fun reload() {
-        loadProperties()
+        return System.getProperty(key, "")
     }
 
 }
