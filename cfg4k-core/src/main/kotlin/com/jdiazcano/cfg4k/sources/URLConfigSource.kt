4ab222812e629ce98ce680d39c98002b5d391d91
Daniil Penkin <dpenkin@atlassian.com>
2018-02-20 16:18:45 +1100

    Authentication for URLConfigSource
    - URLConfigSource now accepts an optional Authorization header content
    - There's also a helper function for encoding basic auth credentials

--- cfg4k-core/src/main/kotlin/com/jdiazcano/cfg4k/sources/URLConfigSource.kt
+++ cfg4k-core/src/main/kotlin/com/jdiazcano/cfg4k/sources/URLConfigSource.kt
@@ -1,10 +1,27 @@
 package com.jdiazcano.cfg4k.sources
 
 import java.io.InputStream
+import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
 
-class URLConfigSource(private val url: URL): ConfigSource {
+class URLConfigSource(private val url: URL,
+                      private val authHeader: String? = null) : ConfigSource {
     override fun read(): InputStream {
-        return url.openStream()
+        val connection = url.openConnection() as HttpURLConnection
+        if (authHeader != null) {
+            connection.setRequestProperty("Authorization", authHeader)
+        }
+
+        return connection.inputStream
     }
+}
+
+/**
+ * Generates `Authorization` header value for basic authentication with the given [username] and [password].
+ */
+fun basicAuth(username: String, password: String): String {
+    val credentials = "$username:$password"
+    val encodedCredentials = Base64.getEncoder().encodeToString(credentials.toByteArray(Charsets.UTF_8))
+    return "Basic $encodedCredentials"
 }
