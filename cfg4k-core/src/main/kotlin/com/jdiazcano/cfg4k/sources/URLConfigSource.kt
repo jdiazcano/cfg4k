db043fcc04b6f28ff4543448ea209e017b14e72d
jdiazcano <terkitos@gmail.com>
2018-02-27 19:50:57 +0000

    Added Bitbucket and Github ConfigSources that doesn't require any dependency

--- cfg4k-core/src/main/kotlin/com/jdiazcano/cfg4k/sources/URLConfigSource.kt
+++ cfg4k-core/src/main/kotlin/com/jdiazcano/cfg4k/sources/URLConfigSource.kt
@@ -17,11 +17,47 @@ class URLConfigSource(private val url: URL,
     }
 }
 
+
+private const val GITHUB_RAW_BASE_URL = "https://raw.githubusercontent.com"
+fun GitHubConfigSource(repoOwner: String,
+                       repoSlug: String,
+                       configFilePath: String,
+                       repoBranch: String = "master"): ConfigSource {
+        return URLConfigSource(URL("$GITHUB_RAW_BASE_URL/$repoOwner/$repoSlug/$repoBranch/$configFilePath"))
+}
+
+
+private const val BITBUCKET_API_BASE_URL = "https://api.bitbucket.org"
+fun BitbucketConfigSource(repoOwner: String,
+                          repoSlug: String,
+                          configFilePath: String,
+                          repoBranch: String = "master",
+                          authHeader: String? = null): ConfigSource {
+        return URLConfigSource(
+                URL("$BITBUCKET_API_BASE_URL/2.0/repositories/$repoOwner/$repoSlug/src/$repoBranch/$configFilePath"),
+                authHeader
+        )
+}
+
+fun BitbucketConfigSource(username: String,
+                          password: String,
+                          repoOwner: String,
+                          repoSlug: String,
+                          configFilePath: String,
+                          repoBranch: String = "master"): ConfigSource {
+    return BitbucketConfigSource(repoOwner, repoSlug, configFilePath, repoBranch, basicAuth(username, password))
+}
+
 /**
  * Generates `Authorization` header value for basic authentication with the given [username] and [password].
  */
 fun basicAuth(username: String, password: String): String {
     val credentials = "$username:$password"
-    val encodedCredentials = Base64.getEncoder().encodeToString(credentials.toByteArray(Charsets.UTF_8))
+    val encodedCredentials = Base64.getEncoder().encodeToString(credentials.toByteArray())
     return "Basic $encodedCredentials"
 }
+
+/**
+ * Generates `Authorization` header value for a token (Used in github for example).
+ */
+fun String.asToken() = "Token $this"
--- cfg4k-core/src/test/kotlin/com/jdiazcano/cfg4k/URLConfigSourceTest.kt
+++ cfg4k-core/src/test/kotlin/com/jdiazcano/cfg4k/URLConfigSourceTest.kt
@@ -16,7 +16,12 @@
 
 package com.jdiazcano.cfg4k
 
+import com.jdiazcano.cfg4k.loaders.PropertyConfigLoader
+import com.jdiazcano.cfg4k.providers.DefaultConfigProvider
+import com.jdiazcano.cfg4k.providers.get
+import com.jdiazcano.cfg4k.sources.GitHubConfigSource
 import com.jdiazcano.cfg4k.sources.URLConfigSource
+import com.jdiazcano.cfg4k.sources.asToken
 import com.jdiazcano.cfg4k.sources.basicAuth
 import com.winterbe.expekt.should
 import okhttp3.mockwebserver.MockResponse
@@ -80,11 +85,25 @@ class URLConfigSourceTest : Spek({
 
     }
 
+    describe("a github config source") {
+        it("should fetch from a public repo") {
+            val configSource = GitHubConfigSource("jdiazcano", "cfg4k", "cfg4k-core/src/test/resources/test.properties")
+            val provider = DefaultConfigProvider(PropertyConfigLoader(configSource))
+            provider.get<String>("a").should.be.equal("b")
+        }
+    }
+
     describe("authentication header generators") {
         it("basic authentication generator") {
             basicAuth("", "").should.be.equal("Basic Og==")
             basicAuth("foo", "bar").should.be.equal("Basic Zm9vOmJhcg==")
             basicAuth("username", "password").should.be.equal("Basic dXNlcm5hbWU6cGFzc3dvcmQ=")
+        }
+    }
+
+    describe("token header generators") {
+        it("token authentication generator") {
+            "mycooltoken".asToken().should.be.equal("Token mycooltoken")
         }
     }
 })
