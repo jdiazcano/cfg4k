1d03bb6a473a647dddc9f5e4382d13d5d660422a
jdiazcano <terkitos@gmail.com>
2018-08-06 19:55:40 +0100

    Added InputConfigSource, from an InputStream you get a ConfigSource

--- cfg4k-core/src/main/kotlin/com/jdiazcano/cfg4k/sources/ConfigSource.kt
+++ cfg4k-core/src/main/kotlin/com/jdiazcano/cfg4k/sources/ConfigSource.kt
@@ -5,3 +5,15 @@ import java.io.InputStream
 interface ConfigSource {
     fun read(): InputStream
 }
+
+/**
+ * A class to just proxy the InputStream to a ConfigSource
+ */
+class InputConfigSource(private val inputStream: InputStream): ConfigSource {
+    override fun read() = inputStream
+}
+
+/**
+ * Convert this InputStream to a ConfigSource
+ */
+fun InputStream.toConfigSource() = InputConfigSource(this)
